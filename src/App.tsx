6239f888e90be5091a330f0c13c960036aa035bd
AlbertPangilinan <rayalbertz@gmail.com>
2024-03-15 19:53:18 -0400

    refactored indentation from 2 space to 4 spaces

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,30 @@ import './App.css';
 import * as projectsAPI from './api/projects'
 
 function App() {
-  const [loaded, setLoaded] = useState(false)
-  const [projects, setProjects] = useState([])
+    const [loaded, setLoaded] = useState(false)
+    const [projects, setProjects] = useState([])
 
-  useEffect(() => {
-    if (!loaded) {
-      projectsAPI.getAllProjects().then((projects) => {
-        setLoaded(true);
-        setProjects(projects);
-      });
-    }
-  });
-
-  function renderProjects() {
-    let entries = projects.map((project : any) => {
-      return <div style={{ border: "1px solid black", margin: "16px" }}>{project.topic + ' - ' + project.name}</div>
+    useEffect(() => {
+        if (!loaded) {
+            projectsAPI.getAllProjects().then((projects) => {
+                setLoaded(true);
+                setProjects(projects);
+            });
+        }
     });
-    return entries
-  }
 
-  return (
-    <div className="app">
-      {renderProjects()}
-    </div>
-  );
+    function renderProjects() {
+        let entries = projects.map((project : any) => {
+            return <div style={{ border: "1px solid black", margin: "16px" }}>{project.topic + ' - ' + project.name}</div>
+        });
+        return entries
+    }
+
+    return (
+        <div className="app">
+            {renderProjects()}
+        </div>
+    );
 }
 
 export default App;
